bdd903d1b29b701c93bf747d25854f56cdb18d52
haka08 <hafsakabeer008@gmail.com>
2021-12-18 05:15:35 +0800

    constructing all functions in js for music player

---
+++ js/musicplayer.js
@@ -0,0 +1,12 @@
+const music = document.querySelector('#audio');
+const seekBar = document.querySelector('.seek-bar');
+const songName = document.querySelector('.music-name');
+const artistName = document.querySelector('.artist-name'); 
+const disk = document.querySelector('.disk');
+const currentTime = document.querySelector('.current-time'); 
+const musicDuration = document.querySelector('.song-duration'); 
+const playBtn = document.querySelector('.play-btn'); 
+const forwardBtn = document.querySelector('.forward-btn'); 
+const backwardBtn = document.querySelector('.backward-btn');
+
+
